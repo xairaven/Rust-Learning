9a35dfee08d42b077f6080d51e162558471efe4a
xairaven <alexkovalevkpi@gmail.com>
2024-06-22 02:30:28 +0300

    (Chapter 12) Reading a File

---
+++ 12-An-IO-Project/02-Reading-a-File/01-Run-Tests.sh
@@ -0,0 +1,8 @@
+cargo run -- the poem.txt
+# Searching for the
+# In file poem.txt
+# With text:
+# I'm nobody! Who are you?
+# Are you nobody, too?
+# Then there's a pair of us - don't tell!
+# They'd banish us, you know.
