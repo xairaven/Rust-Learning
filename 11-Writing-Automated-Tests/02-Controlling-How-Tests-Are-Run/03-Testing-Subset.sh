4dd045a08bd41c9b3727420571e8d2237f969e2f
xairaven <alexkovalevkpi@gmail.com>
2024-06-22 01:28:58 +0300

    (Chapter 11) Controlling How Tests Are Run

---
+++ 11-Writing-Automated-Tests/02-Controlling-How-Tests-Are-Run/03-Testing-Subset.sh
@@ -0,0 +1,8 @@
+# One test tested
+cargo test one_hundred
+
+# Testing two test
+cargo test add
+# running 2 tests
+# test tests::add_three_and_two ... ok
+# test tests::add_two_and_two ... ok
---
+++ 11-Writing-Automated-Tests/02-Controlling-How-Tests-Are-Run/04-Testing-Ignored-Tests.sh
@@ -0,0 +1,5 @@
+# Ignored tests = test with #[ignore] attribute
+cargo test -- --ignored
+
+# Run all tests, include ignored
+cargo test -- --include-ignored
