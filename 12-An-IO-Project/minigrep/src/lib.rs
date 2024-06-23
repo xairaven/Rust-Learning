69744836676540948e74a8b69eb5580d0cd44a72
xairaven <alexkovalevkpi@gmail.com>
2024-06-23 18:12:30 +0300

    (Chapter 12) Developing the Library’s Functionality with Test Driven Development

--- 12-An-IO-Project/minigrep/src/lib.rs
+++ 12-An-IO-Project/minigrep/src/lib.rs
@@ -1,5 +1,8 @@
-use std::{fs};
 use std::error::Error;
+use std::fs;
+
+#[cfg(test)]
+mod lib_tests;
 
 pub struct Config {
     pub query: String,
@@ -22,7 +25,21 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
