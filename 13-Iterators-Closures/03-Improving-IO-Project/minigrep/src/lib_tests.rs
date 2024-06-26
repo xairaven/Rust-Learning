d666bd6a33461c159dbd9d8af6609f6948103da4
xairaven <alexkovalevkpi@gmail.com>
2024-06-26 21:54:11 +0300

    (Chapter 13) Improving Our I/O Project

---
+++ 13-Iterators-Closures/03-Improving-IO-Project/minigrep/src/lib_tests.rs
@@ -0,0 +1,28 @@
+use super::*;
+
+#[test]
+fn one_result() {
+    let query = "duct";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+    assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+}
+
+#[test]
+fn case_insensitive() {
+    let query = "rUsT";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+    assert_eq!(
+        vec!["Rust:", "Trust me."],
+        search_case_insensitive(query, contents)
+    );
+}
