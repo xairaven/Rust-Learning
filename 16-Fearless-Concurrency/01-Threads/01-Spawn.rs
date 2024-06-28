8620476c90bfb3c847a6a06de15e73c8258f266b
xairaven <alexkovalevkpi@gmail.com>
2024-06-29 02:42:47 +0300

    (Chapter 16) Using Threads to Run Code Simultaneously

---
+++ 16-Fearless-Concurrency/01-Threads/01-Spawn.rs
@@ -0,0 +1,28 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {i} from the spawned thread!");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {i} from the main thread!");
+        thread::sleep(Duration::from_millis(1));
+    }
+}
+
+/*
+hi number 1 from the main thread!
+hi number 1 from the spawned thread!
+hi number 2 from the main thread!
+hi number 2 from the spawned thread!
+hi number 3 from the spawned thread!
+hi number 3 from the main thread!
+hi number 4 from the spawned thread!
+hi number 4 from the main thread!
+hi number 5 from the spawned thread!
+ */
