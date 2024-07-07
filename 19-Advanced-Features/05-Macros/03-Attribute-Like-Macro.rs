d4399beb942b19c3cf5259ed9e79d48889c9e9a8
xairaven <alexkovalevkpi@gmail.com>
2024-07-07 13:57:54 +0300

    (Chapter 19) Macros

---
+++ 19-Advanced-Features/05-Macros/03-Attribute-Like-Macro.rs
@@ -0,0 +1,7 @@
+#[route(GET, "/")]
+fn index() {}
+
+/*
+#[proc_macro_attribute]
+pub fn route(attr: TokenStream, item: TokenStream) -> TokenStream {
+ */
---
+++ 19-Advanced-Features/05-Macros/04-Function-Like-Macro.rs
@@ -0,0 +1,6 @@
+let sql = sql!(SELECT * FROM posts WHERE id=1);
+
+/*
+#[proc_macro]
+pub fn sql(input: TokenStream) -> TokenStream {
+ */
